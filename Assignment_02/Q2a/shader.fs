1ad9a57ad19183bc4722129e51f27e90c68fb100
AshishBokil <ashishbkl11@gmail.com>
2021-10-02 16:48:37 +0530

    Q2a Done

--- Assignment_02/Q2a/shader.fs
+++ Assignment_02/Q2a/shader.fs
@@ -3,14 +3,22 @@
 
 
 in float Scalar;
+in vec3 Objcolor;
 
 vec4 colormap(float x) {
 	float d = clamp(x, 0.0, 1.0);
 	//return vec4(d, d, d, 1.0);
-    return vec4(d, d, d, 1.0);
+    return vec4(d, 0, 0, 1.0);
 }
 void main()
 {
    // gl_FragColor=vec4(1.0,1.0,0,1.0);
+   if(Objcolor.x==0){
+		gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+	}
+	else {
+
     gl_FragColor=colormap(Scalar/255);
+	}
+    
 }
--- Assignment_02/Q2a/shader.vs
+++ Assignment_02/Q2a/shader.vs
@@ -3,9 +3,11 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in float aScalar;
 uniform mat4 gWorld;
+uniform vec3 objcolor;
 
 // out vec3 FragPos;  
 out float Scalar;
+out vec3 Objcolor;
 
 void main()
 {  
@@ -14,5 +16,6 @@ void main()
     // gl_Position = vec4(0.5 * aPos, 1.0);
     // FragPos = vec3(gl_Position);
     Scalar = aScalar;
+    Objcolor=objcolor;
     
 }
